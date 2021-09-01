fbe2908c4fd89bf011459e32131c8e6e3bb8362a
karimabuseer <karimabuseer@gmail.com>
2021-09-01 17:01:34 +0100

    Added test to shop spec for sellIn decrease

--- spec/gildedRoseShopSpec.js
+++ spec/gildedRoseShopSpec.js
@@ -3,7 +3,7 @@ describe("Gilded Rose", function() {
   let manaPotionDouble; 
   let gildedRose;
   beforeEach(() => { 
-    manaPotionDouble = { name: 'manaPotion', sellIn: 20, quality: 20 } ;
+    manaPotionDouble = { name: 'manaPotion', sellIn: 10, quality: 20 } ;
     gildedRose = new Shop([manaPotionDouble]);
     items = gildedRose.items;
   })
@@ -13,10 +13,16 @@ describe("Gilded Rose", function() {
   });
 
   describe('Normal items', () => { 
-    it("should have their quality reduced by 1 when SellIn is positive int", () => {
+    it("have their quality reduced by 1 when SellIn is positive int", () => {
       expect(gildedRose.items[0].quality).toEqual(20);
       gildedRose.updateQuality()
       expect(gildedRose.items[0].quality).toEqual(19);
+    })
+
+    it("have their sellIn reduced by 1 when SellIn is positive int", () => {
+      expect(gildedRose.items[0].sellIn).toEqual(10);
+      gildedRose.updateQuality()
+      expect(gildedRose.items[0].sellIn).toEqual(9);
     })
   })
 });
