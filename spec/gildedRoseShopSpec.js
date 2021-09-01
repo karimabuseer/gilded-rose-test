1241145055cd6034aa84dc46976ec0686ba19b4d
karimabuseer <karimabuseer@gmail.com>
2021-09-01 17:11:30 +0100

    Added skeleton for test groups + some semi colons

--- spec/gildedRoseShopSpec.js
+++ spec/gildedRoseShopSpec.js
@@ -6,7 +6,7 @@ describe("Gilded Rose", function() {
     manaPotionDouble = { name: 'manaPotion', sellIn: 10, quality: 20 } ;
     gildedRose = new Shop([manaPotionDouble]);
     items = gildedRose.items;
-  })
+  });
 
   it("should know details about its stock", function() {
     expect(items[0].name).toEqual("manaPotion");
@@ -15,14 +15,26 @@ describe("Gilded Rose", function() {
   describe('Normal items', () => { 
     it("have their quality reduced by 1 when SellIn is positive int", () => {
       expect(gildedRose.items[0].quality).toEqual(20);
-      gildedRose.updateQuality()
+      gildedRose.updateQuality();
       expect(gildedRose.items[0].quality).toEqual(19);
-    })
+    });
 
     it("have their sellIn reduced by 1 when SellIn is positive int", () => {
       expect(gildedRose.items[0].sellIn).toEqual(10);
-      gildedRose.updateQuality()
+      gildedRose.updateQuality();
       expect(gildedRose.items[0].sellIn).toEqual(9);
-    })
+    });
   })
+
+  describe('Aged Brie', () => {
+
+  });
+
+  describe('Legendary items', () => {
+    
+  });
+
+  describe('Backstage Passes', () => {
+    
+  });
 });
