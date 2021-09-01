ae9651ef028e0248a71a2aafe48881e0548fc397
karimabuseer <karimabuseer@gmail.com>
2021-09-01 17:00:08 +0100

    Added test that checks to see if regular item has quality downgraded after a day

--- spec/gildedRoseShopSpec.js
+++ spec/gildedRoseShopSpec.js
@@ -1,14 +1,22 @@
 Shop =  require('../src/gildedRoseShop.js').Shop;
 describe("Gilded Rose", function() {
   let manaPotionDouble; 
+  let gildedRose;
   beforeEach(() => { 
     manaPotionDouble = { name: 'manaPotion', sellIn: 20, quality: 20 } ;
+    gildedRose = new Shop([manaPotionDouble]);
+    items = gildedRose.items;
   })
 
   it("should know details about its stock", function() {
-    // console.log(Shop)
-    const gildedRose = new Shop([manaPotionDouble]);
-    const items = gildedRose.items;
     expect(items[0].name).toEqual("manaPotion");
   });
+
+  describe('Normal items', () => { 
+    it("should have their quality reduced by 1 when SellIn is positive int", () => {
+      expect(gildedRose.items[0].quality).toEqual(20);
+      gildedRose.updateQuality()
+      expect(gildedRose.items[0].quality).toEqual(19);
+    })
+  })
 });
