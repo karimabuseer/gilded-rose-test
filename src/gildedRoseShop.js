0ab230d22f5972809431021317e2fb22b6016df2
karimabuseer <karimabuseer@gmail.com>
2021-09-03 10:27:34 +0100

    Added a call to regular quality lower on line 30

--- src/gildedRoseShop.js
+++ src/gildedRoseShop.js
@@ -27,7 +27,7 @@ class Shop {
           if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.quality > 0) {
               if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1;
+                this.regularLowerQualityByOne(item)
               }
             }
           } else {
